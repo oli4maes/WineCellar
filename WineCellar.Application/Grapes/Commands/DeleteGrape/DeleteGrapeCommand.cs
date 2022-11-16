e11d5a722c7e7b72f87034c5e203d3741b8b0c32
Olivier Maes <olivier.maes@outlook.com>
2022-11-16 20:53:22 +0100

    Added DeleteGrapeCommand and UpdateGrapeCommand

---
+++ WineCellar.Application/Grapes/Commands/DeleteGrape/DeleteGrapeCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace WineCellar.Application.Grapes.Commands.DeleteGrape;
+
+public record DeleteGrapeCommand(int id) : IRequest<bool>;
+
+public class DeleteGrapeHandler : IRequestHandler<DeleteGrapeCommand, bool>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteGrapeHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(DeleteGrapeCommand request, CancellationToken cancellationToken)
+    {
+        bool succes = await _unitOfWork.Grapes.Delete(request.id);
+
+        await _unitOfWork.CompleteAsync();
+
+        return succes;
+    }
+}
+
---
+++ WineCellar.Application/Grapes/Commands/UpdateGrape/UpdateGrapeCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace WineCellar.Application.Grapes.Commands.UpdateGrape;
+
+public record UpdateGrapeCommand(Grape grape) : IRequest;
+
+public class UpdateGrapeHandler : IRequestHandler<UpdateGrapeCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateGrapeHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(UpdateGrapeCommand request, CancellationToken cancellationToken)
+    {
+        await _unitOfWork.Grapes.Update(request.grape);
+        await _unitOfWork.CompleteAsync();
+
+        return Unit.Value;
+    }
+}
