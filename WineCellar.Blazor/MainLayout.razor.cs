f3d8bbfbbbfa91298822e34c8e8272938fb383a2
Olivier Maes <olivier.maes@outlook.com>
2022-11-17 15:41:11 +0100

    Added dark theme

--- WineCellar.Blazor/MainLayout.razor.cs
+++ WineCellar.Blazor/MainLayout.razor.cs
@@ -3,6 +3,17 @@
 public partial class MainLayout
 {
     bool _drawerOpen = false;
+    bool _isDarkMode;
+    MudThemeProvider _mudThemeProvider;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+            StateHasChanged();
+        }
+    }
 
     void DrawerToggle()
     {
