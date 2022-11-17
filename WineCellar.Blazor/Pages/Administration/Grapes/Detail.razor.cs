d58ae8a1a9e1266b908b0528bb089f36fedd73c8
Olivier Maes <olivier.maes@outlook.com>
2022-11-17 09:16:31 +0100

    Added grape detail page

--- WineCellar.Blazor/Pages/Administration/Grapes/Detail.razor.cs
+++ WineCellar.Blazor/Pages/Administration/Grapes/Detail.razor.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using WineCellar.Application.Grapes.Commands.CreateGrape;
+using WineCellar.Application.Grapes.Commands.UpdateGrape;
 using WineCellar.Application.Grapes.Queries.GetGrapeById;
 
 namespace WineCellar.Blazor.Pages.Administration.Grapes;
@@ -10,13 +13,60 @@ public partial class Detail : ComponentBase
     [Inject]
     MediatR.IMediator _mediator { get; set; }
 
-    private Grape _grape = new();
+    [Inject]
+    private NavigationManager _navManager { get; set; }
+
+    [Inject]
+    private ISnackbar _snackbar { get; set; }
+
+    private Grape _grape { get; set;} = new();
+    private bool _editMode { get; set; } = false;
 
     protected override async Task OnInitializedAsync()
     {
         if (Id != 0)
         {
             _grape = await _mediator.Send(new GetGrapeByIdQuery(Id));
-        }        
+        }
+        else
+        {
+            _grape = new();
+            _editMode = true;
+        }
+    }
+
+    private void SetEditMode()
+    {
+        _editMode = true;
+    }
+
+    protected async void HandleValidSubmit()
+    {
+        if (Id == 0) // Insert
+        {
+            _grape = await _mediator.Send(new CreateGrapeCommand(_grape.Name, _grape.Description));
+
+            if (_grape is not null)
+            {
+                _editMode = false;
+                _snackbar.Add("Saved", Severity.Success);                
+            }
+            else
+            {
+                _snackbar.Add("Could not save the grape.", Severity.Error);
+            }
+        }
+        else // Update
+        {
+            await _mediator.Send(new UpdateGrapeCommand(_grape));
+
+            _editMode = false;
+            _snackbar.Add("Saved", Severity.Success);            
+        }
+    }
+
+    private void Back()
+    {
+        _navManager.NavigateTo("/Administration/Grapes");
     }
 }
