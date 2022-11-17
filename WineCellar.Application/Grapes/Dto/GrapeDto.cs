1f3806c503d24773b725e370c40b21cb25186bdf
Olivier Maes <olivier.maes@outlook.com>
2022-11-17 15:35:50 +0100

    Added basic validation

--- WineCellar.Application/Grapes/Dto/GrapeDto.cs
+++ WineCellar.Application/Grapes/Dto/GrapeDto.cs
@@ -1,8 +1,15 @@
-﻿namespace WineCellar.Application.Grapes.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineCellar.Application.Grapes.Dto;
 
 public class GrapeDto
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50, ErrorMessage = "Name is too long (50 characters limit).")]
     public string Name { get; set; } = String.Empty;
+
+    [StringLength(1000, ErrorMessage = "Description is too long (1000 characters limit).")]
     public string? Description { get; set; }
 }
