4f96a1e02e3b73a0a93e9abbe6f10aa7e6b87311
Olivier Maes <olivier.maes@outlook.com>
2022-11-18 15:40:36 +0100

    Added check if user is admin

--- WineCellar.Blazor/ConfigureServices.cs
+++ WineCellar.Blazor/ConfigureServices.cs
@@ -1,4 +1,6 @@
-﻿using Auth0.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using MudBlazor.Services;
 
 namespace WineCellar.Blazor;
@@ -8,11 +10,70 @@ public static class ConfigureServices
     public static IServiceCollection AddBlazorServices(this IServiceCollection services, IConfiguration configuration)
     {
         // Auth0
-        services.AddAuth0WebAppAuthentication(options =>
+        services.AddAuthentication(options => 
         {
-            options.Domain = configuration["Auth0:Domain"];
-            options.ClientId = configuration["Auth0:ClientId"];
-        });
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        })
+            .AddCookie()
+            .AddOpenIdConnect("Auth0", options =>
+            {
+                // Set the authority to your Auth0 domain
+                options.Authority = $"https://{configuration["Auth0:Domain"]}";
+
+                // Configure the Auth0 Client ID and Client Secret
+                options.ClientId = configuration["Auth0:ClientId"];
+                options.ClientSecret = configuration["Auth0:ClientSecret"];
+
+                // Set response type to code
+                options.ResponseType = "code";
+
+                // Configure the scope
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("email");
+
+                // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+                options.CallbackPath = new PathString("/callback");
+
+                // Configure the Claims Issuer to be Auth0
+                options.ClaimsIssuer = "Auth0";
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "https://schemas.dev-wx1wwh5a8jh6uhho.com/roles"
+                };
+
+                options.Events = new OpenIdConnectEvents
+                {
+                    // handle the logout redirection
+                    OnRedirectToIdentityProviderForSignOut = (context) =>
+                    {
+                        var logoutUri = $"https://{configuration["Auth0:Domain"]}/v2/logout?client_id={configuration["Auth0:ClientId"]}";
+
+                        var postLogoutUri = context.Properties.RedirectUri;
+                        if (!string.IsNullOrEmpty(postLogoutUri))
+                        {
+                            if (postLogoutUri.StartsWith("/"))
+                            {
+                                // transform to absolute
+                                var request = context.Request;
+                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                            }
+                            logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+                        }
+
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
 
         services.AddRazorPages();
         services.AddServerSideBlazor();
--- WineCellar.Blazor/MainLayout.razor.cs
+++ WineCellar.Blazor/MainLayout.razor.cs
@@ -2,7 +2,11 @@
 
 public partial class MainLayout
 {
+    [Inject]
+    private AuthenticationStateProvider _authStateProvider { get; set; }
+
     bool _drawerOpen = false;
+    bool _isAdmin = false;
     bool _isDarkMode;
     MudThemeProvider _mudThemeProvider;
 
@@ -12,6 +16,22 @@ public partial class MainLayout
         {
             _isDarkMode = await _mudThemeProvider.GetSystemPreference();
             StateHasChanged();
+        }
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await _authStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity.IsAuthenticated)
+        {            
+            var claims = user.Identities.First().Claims;
+
+            if (claims.Any(x => x.Value == "admin"))
+            {
+                _isAdmin= true;
+            }
         }
     }
 
