35085984bd748a6e698aa155c6133ff0cbec1ff6
Olivier Maes <olivier.maes@outlook.com>
2022-11-22 09:17:19 +0100

    More policy based authorization changes

--- WineCellar.Blazor/Shared/MainLayout.razor.cs
+++ WineCellar.Blazor/Shared/MainLayout.razor.cs
@@ -2,9 +2,6 @@
 
 public partial class MainLayout
 {
-    [Inject]
-    private AuthenticationStateProvider _authStateProvider { get; set; }
-
     bool _drawerOpen = false;
     bool _isAdmin = false;
     bool _isDarkMode;
@@ -16,22 +13,6 @@ public partial class MainLayout
         {
             _isDarkMode = await _mudThemeProvider.GetSystemPreference();
             StateHasChanged();
-        }
-    }
-
-    protected override async Task OnInitializedAsync()
-    {
-        var authState = await _authStateProvider.GetAuthenticationStateAsync();
-        var user = authState.User;
-
-        if (user.Identity.IsAuthenticated)
-        {            
-            var claims = user.Identities.First().Claims;
-
-            if (claims.Any(x => x.Value == "admin"))
-            {
-                _isAdmin= true;
-            }
         }
     }
 
