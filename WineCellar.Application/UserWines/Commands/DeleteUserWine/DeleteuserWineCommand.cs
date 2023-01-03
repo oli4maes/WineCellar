f95aea69b36e2d26fb35b854b122f2ce35a62bf0
Olivier Maes <olivier.maes@outlook.com>
2023-01-03 20:12:24 +0100

    Added UserWine Commands

---
+++ WineCellar.Application/UserWines/Commands/DeleteUserWine/DeleteuserWineCommand.cs
@@ -0,0 +1,22 @@
+namespace WineCellar.Application.UserWines.Commands.DeleteUserWine;
+
+public sealed record DeleteUserWineCommand(int Id) : IRequest<bool>;
+
+public sealed class DeleteUserWineHandler : IRequestHandler<DeleteUserWineCommand, bool> 
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteUserWineHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(DeleteUserWineCommand request, CancellationToken cancellationToken)
+    {
+        bool succes = await _unitOfWork.UserWines.Delete(request.Id);
+
+        await _unitOfWork.CompleteAsync();
+
+        return succes;
+    }
+} 
