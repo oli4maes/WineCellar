2227f7b2b6e0366b13013ae85256397d0936ea50
Olivier Maes <olivier.maes@ugentec.com>
2023-02-27 13:41:24 +0100

    #43 started working on a custom theme

--- WineCellar.Blazor/Shared/MainLayout.razor.cs
+++ WineCellar.Blazor/Shared/MainLayout.razor.cs
@@ -4,9 +4,44 @@ public partial class MainLayout
 {
     private bool _drawerOpen = true;
     private bool _isDarkMode;
-    private DrawerClipMode _clipMode = DrawerClipMode.Always;
-    
+    private readonly DrawerClipMode _clipMode = DrawerClipMode.Always;
     private MudThemeProvider _mudThemeProvider;
+
+    private readonly MudTheme _theme = new()
+    {
+        Typography = new Typography()
+        {
+            Default = new Default()
+            {
+                FontFamily = new[] { "Open Sans", "Helvetica", "Arial", "sans-serif" },
+                FontWeight = 400
+            },
+            H1 = new H1()
+            {
+                FontFamily = new[] { "Fira Sans", "Helvetica", "Arial", "sans-serif" }
+            },
+            H2 = new H2()
+            {
+                FontFamily = new[] { "Fira Sans", "Helvetica", "Arial", "sans-serif" }
+            },
+            H3 = new H3()
+            {
+                FontFamily = new[] { "Fira Sans", "Helvetica", "Arial", "sans-serif" }
+            },
+            H4 = new H4()
+            {
+                FontFamily = new[] { "Fira Sans", "Helvetica", "Arial", "sans-serif" }
+            },
+            H5 = new H5()
+            {
+                FontFamily = new[] { "Fira Sans", "Helvetica", "Arial", "sans-serif" }
+            },
+            H6 = new H6()
+            {
+                FontFamily = new[] { "Fira Sans", "Helvetica", "Arial", "sans-serif" }
+            },
+        }
+    };
 
     // protected override async Task OnAfterRenderAsync(bool firstRender)
     // {
