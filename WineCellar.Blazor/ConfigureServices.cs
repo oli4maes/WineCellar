a2d94cb9a7d73321b970969f662979cc57e4f525
Olivier Maes <olivier.maes@outlook.com>
2023-03-03 21:09:23 +0100

    Changed basic layout

--- WineCellar.Blazor/ConfigureServices.cs
+++ WineCellar.Blazor/ConfigureServices.cs
@@ -23,7 +23,19 @@ public static class ConfigureServices
         services.AddRazorPages();
         services.AddServerSideBlazor();
 
-        services.AddMudServices();
+        services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 10000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Text;
+            }
+        );
 
         return services;
     }
