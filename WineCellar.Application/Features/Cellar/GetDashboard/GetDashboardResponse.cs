a8b197fae1ef69ca391ebdc84fbba87b072e843f
Olivier Maes <olivier.maes@ugentec.com>
2023-03-24 15:44:27 +0100

    #18 Started working on dashboard

--- WineCellar.Application/Features/Cellar/GetDashboard/GetDashboardResponse.cs
+++ WineCellar.Application/Features/Cellar/GetDashboard/GetDashboardResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace WineCellar.Application.Features.Cellar.GetDashboard;
+﻿using WineCellar.Domain.Enums;
+
+namespace WineCellar.Application.Features.Cellar.GetDashboard;
 
 public sealed class GetDashboardResponse
 {
     public int AmountOfBottlesInCellar { get; set; }
     public double[]? AmountOfBottlesPerWineTypeData { get; set; }
     public string[]? AmountOfBottlesPerWineTypeLabels { get; set; }
+    public WineType FavouriteWineType { get; set; }
 }
