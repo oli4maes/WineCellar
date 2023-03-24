3f3182d6fef7fe28faaf01d2a16ae7b075e9898d
Olivier Maes <olivier.maes@ugentec.com>
2023-03-24 15:17:17 +0100

    Started working on dashboard

---
+++ WineCellar.Blazor/Features/Cellar/Components/WineTypeDonut.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace WineCellar.Blazor.Features.Cellar.Components;
+
+public partial class WineTypeDonut : ComponentBase
+{
+    [Parameter] public double[] AmountOfBottlesPerWineTypeData { get; set; }
+    [Parameter] public string[] AmountOfBottlesPerWineTypeLabels { get; set; }
+    [Parameter] public int AmountOfBottlesInCellar { get; set; }
+
+    private int _index = -1;
+    private ChartOptions _chartOptions { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _chartOptions = new ChartOptions()
+        {
+            ChartPalette = new[]
+                { Colors.Yellow.Lighten1, Colors.Red.Lighten3, Colors.Red.Darken4, Colors.Amber.Lighten1 }
+        };
+    }
+}
