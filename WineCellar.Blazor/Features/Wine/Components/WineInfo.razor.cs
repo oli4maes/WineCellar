52a9b032c58085e120278d49adebba789d3fc70f
Olivier Maes <olivier.maes@outlook.com>
2023-04-01 14:15:25 +0200

    #72 UI UX Improvements

--- WineCellar.Blazor/Features/Wine/Components/WineInfo.razor.cs
+++ WineCellar.Blazor/Features/Wine/Components/WineInfo.razor.cs
@@ -3,4 +3,10 @@
 public partial class WineInfo : ComponentBase
 {
     [Parameter] public WineDto Wine { get; set; } = new();
+    [Parameter] public EventCallback OnNavigateToWinery { get; set; }
+
+    private async Task NavigateToWinery()
+    {
+        await OnNavigateToWinery.InvokeAsync();
+    }
 }
