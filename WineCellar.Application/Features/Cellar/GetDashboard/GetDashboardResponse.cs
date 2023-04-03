d0a3044cf69166256982ab6b546450463b455c39
Olivier Maes <olivier.maes@ugentec.com>
2023-04-03 09:15:56 +0200

    Fixes #72 Improved on UI UX

--- WineCellar.Application/Features/Cellar/GetDashboard/GetDashboardResponse.cs
+++ WineCellar.Application/Features/Cellar/GetDashboard/GetDashboardResponse.cs
@@ -7,7 +7,7 @@ public sealed class GetDashboardResponse
     public int AmountOfBottlesInCellar { get; set; }
     public double[]? AmountOfBottlesPerWineTypeData { get; set; }
     public string[]? AmountOfBottlesPerWineTypeLabels { get; set; }
-    public WineType FavouriteWineType { get; set; }
+    public WineType? FavouriteWineType { get; set; }
     public Dictionary<WineType, double>? AmountOfBottlesPerWineType { get; set; }
     public string FavouriteWine { get; set; } = String.Empty;
     public int? FavouriteWineId { get; set; }
