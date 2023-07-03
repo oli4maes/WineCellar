82ac0931c043d407c947f33083c7cda3dd7c689d
Olivier Maes <olivier.maes@ugentec.com>
2023-07-03 13:32:30 +0200

    Added automatic migration

--- WineCellar.Blazor/Program.cs
+++ WineCellar.Blazor/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using WineCellar.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,13 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    // Migrate latest database changes during startup
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<ApplicationDbContext>();
+
+    dbContext.Database.Migrate();
 }
 
 app.UseSerilogRequestLogging();
