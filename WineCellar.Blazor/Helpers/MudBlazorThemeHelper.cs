12802baba2110e658eb84d79f18c8fc527a38f80
Olivier Maes <olivier.maes@ugentec.com>
2023-10-19 13:41:48 +0200

    Fixes #134 Implemented a helper for MudBlazor theme

---
+++ WineCellar.Blazor/Helpers/MudBlazorThemeHelper.cs
@@ -0,0 +1,65 @@
+﻿namespace WineCellar.Blazor.Helpers;
+
+public class MudBlazorThemeHelper : IMudBlazorThemeHelper
+{
+    public MudTheme GetTheme()
+    {
+        return new MudTheme()
+        {
+            Typography = new Typography()
+            {
+                Default = new Default()
+                {
+                    FontFamily = new[] { "Open Sans", "Helvetica", "Arial", "sans-serif" },
+                    FontWeight = 500
+                },
+                H1 = new H1() // ONLY USED FOR LOGIN SCREEN
+                {
+                    FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
+                    FontWeight = 900
+                },
+                H2 = new H2() // ONLY USED FOR APPBAR LOGO
+                {
+                    FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
+                    FontWeight = 900,
+                    FontSize = "2.125rem"
+                },
+                H3 = new H3()
+                {
+                    FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
+                    FontWeight = 200
+                },
+                H4 = new H4() // USED FOR PAGE HEADERS
+                {
+                    FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
+                    FontWeight = 300
+                },
+                H5 = new H5() // USED FOR CARD HEADERS
+                {
+                    FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
+                    FontWeight = 400
+                },
+                H6 = new H6()
+                {
+                    FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
+                }
+            },
+            Palette = new PaletteLight
+            {
+                DrawerBackground = "#fbfbfc",
+                TextPrimary = "#5f4d46",
+                Background = "#fbfbfc",
+                AppbarBackground = "#fbfbfc",
+                Primary = "#f1511b"
+            }
+        };
+    }
+
+    public object GetSharedAppData()
+    {
+        return new Dictionary<string, object>
+        {
+            { "Title", "MudTheme Helper!" }
+        };
+    }
+}
--- WineCellar.Blazor/Shared/MainLayout.razor.cs
+++ WineCellar.Blazor/Shared/MainLayout.razor.cs
@@ -1,61 +1,21 @@
-﻿namespace WineCellar.Blazor.Shared;
+﻿using WineCellar.Blazor.Helpers;
+
+namespace WineCellar.Blazor.Shared;
 
 public partial class MainLayout
 {
+    [Inject] public IMudBlazorThemeHelper ThemeHelper { get; set; }
+
     private bool _drawerOpen = true;
     private bool _isDarkMode;
     private readonly DrawerClipMode _clipMode = DrawerClipMode.Always;
-    private MudThemeProvider? _mudThemeProvider;
+    private MudThemeProvider? _mudThemeProvider = new MudThemeProvider();
+    private MudTheme _theme;
 
-    private readonly MudTheme _theme = new()
+    protected override async Task OnInitializedAsync()
     {
-        Typography = new Typography()
-        {
-            Default = new Default()
-            {
-                FontFamily = new[] { "Open Sans", "Helvetica", "Arial", "sans-serif" },
-                FontWeight = 500
-            },
-            H1 = new H1() // ONLY USED FOR LOGIN SCREEN
-            {
-                FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
-                FontWeight = 900
-            },
-            H2 = new H2() // ONLY USED FOR APPBAR LOGO
-            {
-                FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
-                FontWeight = 900,
-                FontSize = "2.125rem"
-            },
-            H3 = new H3()
-            {
-                FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
-                FontWeight = 200
-            },
-            H4 = new H4() // USED FOR PAGE HEADERS
-            {
-                FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
-                FontWeight = 300
-            },
-            H5 = new H5() // USED FOR CARD HEADERS
-            {
-                FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
-                FontWeight = 400
-            },
-            H6 = new H6()
-            {
-                FontFamily = new[] { "Lato", "Helvetica", "Arial", "sans-serif" },
-            }
-        },
-        Palette = new PaletteLight
-        {
-            DrawerBackground = "#fbfbfc",
-            TextPrimary = "#5f4d46",
-            Background = "#fbfbfc",
-            AppbarBackground = "#fbfbfc",
-            Primary = "#f1511b"
-        }
-    };
+        _theme = ThemeHelper.GetTheme();
+    }
 
     void DrawerToggle()
     {
